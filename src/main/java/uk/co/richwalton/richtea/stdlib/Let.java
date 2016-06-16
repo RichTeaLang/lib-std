7ddf2619da766d83f0deab65ec666dd80ab86be3
Richard Walton <walton909@gmail.com>
2016-06-17 00:31:53 +0100

    Let updated to:
     - Allow multiple variable declarations `Let(x:1 y:2)`
     - Allow branch set variables `Let(x: { Return(10) })`
     - Set variables on unbound scope (Rather than blindly on parent scope)

--- src/main/java/uk/co/richwalton/richtea/stdlib/Let.java
+++ src/main/java/uk/co/richwalton/richtea/stdlib/Let.java
@@ -1,32 +1,76 @@
 package uk.co.richwalton.richtea.stdlib;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import richTea.compiler.bootstrap.BindingNode;
+import richTea.runtime.attribute.Attribute;
 import richTea.runtime.attribute.PrimativeAttribute;
 import richTea.runtime.execution.AbstractFunction;
+import richTea.runtime.execution.ReturnException;
 import richTea.runtime.execution.VariableScope;
+import richTea.runtime.functions.Scope;
+import richTea.runtime.node.Branch;
+import richTea.runtime.node.TreeNode;
 
 public class Let extends AbstractFunction {
 
 	@Override
 	protected void run() throws Exception {
-		VariableScope scope = context.getCurrentScope().getParent();
+		VariableScope scope = context.getParentScope(Scope.class);
 		
 		if(scope != null) {
-			String variableName = getVariableName();
-			Object value = getVariableValue();
+			TreeNode currentNode = context.getCurrentNode();
+			Map<String, Attribute> attributes = new HashMap<>();
+
+			// Execute branches and take the last returned value
+			for(Branch branch : currentNode.getBranches()) {
+				String branchName = branch.getName();
+				try {
+					context.executeBranch(branchName);
+				} catch(ReturnException e) {
+					context.unRollScopeTo(this);
+				}
+				
+				attributes.put(branchName, new PrimativeAttribute(branchName, context.getLastReturnValue()));
+			}
 			
-			scope.setAttribute(new PrimativeAttribute(variableName, value));
+			BindingNode binding = currentNode.getBinding();
 			
-			context.setLastReturnValue(value);
+			for(Attribute attribute : currentNode.getAttributes()) {
+				String attributeName = attribute.getName();
+				Attribute existingAttribute = attributes.put(attribute.getName(), attribute);
+				
+				if (existingAttribute != null) {
+					String functionName = binding.getName();
+					String message =  "cannot contain attribute and branch with duplicate name";
+					throw new IllegalArgumentException(functionName + " " + message + ": " + attributeName);
+				}
+			}
+			
+			// Binding default attributes will be set only if an attribute/branch was not present with same name
+			for(Attribute attribute : binding.getDefaultAttributes().getAttributes()) {
+				String attributeName = attribute.getName();
+				
+				if (!attributes.containsKey(attributeName)) {
+					attributes.put(attributeName, attribute);
+				}
+			}
+			
+			List<Attribute> lets = new ArrayList<>(attributes.size());
+			
+			for(Attribute attribute : attributes.values()) {
+				attribute = new PrimativeAttribute(attribute.getName(), attribute.getValue(context));
+				
+				lets.add(attribute);
+				scope.setAttribute(attribute);
+			}
+			
+			context.setLastReturnValue(lets);
 		} else {
 			throw new IllegalArgumentException("No scope to create variable");
 		}
-	}
-	
-	protected String getVariableName() {
-		return context.getString("var");
-	}
-
-	protected Object getVariableValue() {
-		return context.getValue("be");
 	}
 }
